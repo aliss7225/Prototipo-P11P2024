374459da8345e09904973e8f477a2aa328b681fb
Alisson López <156858728+aliss7225@users.noreply.github.com>
2024-02-28 10:12:15 -0600

    Add files via upload

--- Prototipo P11P2024/main.cpp
+++ Prototipo P11P2024/main.cpp
@@ -20,3 +20,50 @@ void llenarMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1]);
 void imprimirMatrizLinea();
 float imprimirMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1], char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA], string nombreFacultad);
 int main()
+{
+    srand(getpid());
+    llamaCiclo();
+    return 0;
+}
+void llamaCiclo()
+{
+    //datos flotantes que determinan la cantidad de matrices que se van a diseñar y los datos que estos deben contener
+    float matriz_facultad_1[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    float matriz_facultad_2[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    float matriz_facultad_3[NUMERO_ALUMNOS][NUMERO_NOTAS + 1];
+    char opcion;
+    bool repetir = true;
+
+    float promedio_facultad_1;
+    float promedio_facultad_2;
+    float promedio_facultad_3;
+
+    char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA] = {"Oliver","Zeta","Adam","Andy","Harry"};
+    do
+    {
+        system("cls");
+        promedio_facultad_1=0;
+        promedio_facultad_2=0;
+        promedio_facultad_3=0;
+        cout << "*** Comparativo de Facultades ***" << endl << endl;
+        llenarMatriz(matriz_facultad_1);
+        promedio_facultad_1 = imprimirMatriz(matriz_facultad_1, alumnos, "Facultad de Ingenieria");
+        llenarMatriz(matriz_facultad_2);
+        promedio_facultad_2 = imprimirMatriz(matriz_facultad_2, alumnos, "Facultad de Arquitectura");
+        llenarMatriz(matriz_facultad_3);
+        promedio_facultad_3 = imprimirMatriz(matriz_facultad_3, alumnos, "Facultad de Administracion");
+        if (promedio_facultad_1 > promedio_facultad_2 && promedio_facultad_1 > promedio_facultad_3)
+        {
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Ingenieria" << " Promedio: " << promedio_facultad_1 << endl;
+        } else
+        if (promedio_facultad_2 > promedio_facultad_1 && promedio_facultad_2 > promedio_facultad_3)
+        {
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Arquitectura" << " Promedio: " << promedio_facultad_2 << endl;
+        } else
+        if (promedio_facultad_3 > promedio_facultad_2 && promedio_facultad_3 > promedio_facultad_1)
+        {
+            cout << " La facultad con el mejor promedio es : " << "Facultad de Administracion" << " Promedio: " << promedio_facultad_3 << endl;
+        } else
+        {
+            cout << " Algunas facultades tienen el mismo promedio " << endl << endl;
+        }
